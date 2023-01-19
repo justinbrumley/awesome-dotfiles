0f88d771076d2fd141c1f32c8d18675140781d5b
Justin Brumley <justin@megahook.in>
2023-01-19 16:45:25 -0600

    change wallpaper

--- awesome/.config/awesome/configuration/wallpaper.lua
+++ awesome/.config/awesome/configuration/wallpaper.lua
@@ -8,7 +8,7 @@ local theme_path = string.format("%s/.config/awesome/theme/", os.getenv("HOME"))
 
 -- Possible Wallpapers: anime.png, waves.jpg, desert.png, scenary.png
 
-local primary_wallpaper = theme_path.."desert.png"
+local primary_wallpaper = theme_path.."street_blues.png"
 local secondary_wallpaper = theme_path.."scenary.png"
 -- ============================================
 
