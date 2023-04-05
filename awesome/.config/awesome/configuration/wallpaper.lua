b7065a02d5cdd015a5de32c387c72e9536acd584
Justin Brumley <justin@megahook.in>
2023-04-05 16:10:43 -0500

    chore: update wallpaper and remove neovim config

--- awesome/.config/awesome/configuration/wallpaper.lua
+++ awesome/.config/awesome/configuration/wallpaper.lua
@@ -8,7 +8,7 @@ local theme_path = string.format("%s/.config/awesome/theme/", os.getenv("HOME"))
 
 -- Possible Wallpapers: anime.png, waves.jpg, desert.png, scenary.png
 
-local primary_wallpaper = theme_path.."archlinux.png"
+local primary_wallpaper = theme_path.."do-something-great.png"
 local secondary_wallpaper = theme_path.."scenary.png"
 -- ============================================
 
